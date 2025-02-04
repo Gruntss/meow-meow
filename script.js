c34bc221d958dbc75d9650136f06c01790936f81
Gruntss <159542316+Gruntss@users.noreply.github.com>
2025-02-04 17:06:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const titleElement = document.querySelector(".title");
 const buttonsContainer = document.querySelector(".buttons");
 const yesButton = document.querySelector(".btn--yes");
 const noButton = document.querySelector(".btn--no");
-const catImg = document.querySelector(".cat-img");  // This is the correct reference
+const catsImg = document.querySelector(".cats-img");  // This is the correct reference
 
-const MAX_IMAGES = 5;
+const MAX_IMAGES = 9;
 
 let play = true;
 let noCount = 0;
