dd6b7c528f20477dfe0ba0445e903329626067d7
Gruntss <159542316+Gruntss@users.noreply.github.com>
2025-02-04 17:36:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,54 +10,65 @@ const MAX_IMAGES = 9;
 
 let play = true;
 let noCount = 0;
+const messages = [
+    "Really?",
+    "Pookie Please",
+    "I can't take this. 🙁",
+    "You're breaking my heart",
+    "Can't you change your mind?",
+    "How can we be sure if we don't try?",
+    "ako nalang sana",
+    "It hurts",
+    "Miss ganda, sige na"
+];
 
 yesButton.addEventListener("click", handleYesClick);
 
 noButton.addEventListener("click", function () {
-  if (play) {
-    noCount++;
-    const imageIndex = Math.min(noCount, MAX_IMAGES);
-    changeImage(imageIndex);
-    resizeYesButton();
-    updateNoButtonText();
-    if (noCount === MAX_IMAGES) {
-      play = false;
+    if (play) {
+        noCount++;
+        const imageIndex = Math.min(noCount, MAX_IMAGES);
+        changeImage(imageIndex);
+        resizeYesButton();
+        updateNoButtonText();
+
+        if (noCount === messages.length) { // Check if last message is displayed
+            noButton.textContent = "Yes, please!"; // Change no button to yes
+            noButton.removeEventListener("click", arguments.callee); // Remove current listener
+            noButton.addEventListener("click", handleYesClick); // Add yes click listener
+        }
+
+        if (noCount > messages.length && noCount <= MAX_IMAGES){
+            if (noCount === MAX_IMAGES) {
+                play = false;
+                titleElement.innerHTML = "Okay fine! :("; // Or whatever message you want
+                buttonsContainer.classList.add("hidden");
+            }
+        }
+
     }
-  }
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
-  buttonsContainer.classList.add("hidden");
-  changeImage("yes");
+    titleElement.innerHTML = "Yayyy!! :3";
+    buttonsContainer.classList.add("hidden");
+    changeImage("yes");
 }
 
 function resizeYesButton() {
-  const computedStyle = window.getComputedStyle(yesButton);
-  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
+    const computedStyle = window.getComputedStyle(yesButton);
+    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+    const newFontSize = fontSize * 1.6;
 
-  yesButton.style.fontSize = `${newFontSize}px`;
-}
-
-function generateMessage(noCount) {
-  const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
-  ];
-
-  const messageIndex = Math.min(noCount, messages.length - 1);
-  return messages[messageIndex];
+    yesButton.style.fontSize = `${newFontSize}px`;
 }
 
 function changeImage(image) {
-  catImg.src = `img/cat-${image}.jpg`;
+    catImg.src = `img/cat-${image}.jpg`;
 }
 
 function updateNoButtonText() {
-  noButton.innerHTML = generateMessage(noCount);
+    if (noCount < messages.length) {
+        noButton.innerHTML = messages[noCount % messages.length]; // Cycle through messages
+    }
 }
